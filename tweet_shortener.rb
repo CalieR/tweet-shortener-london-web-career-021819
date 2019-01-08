7fcd12d20d18ee57486435cc4808aebbd5755f00
Calie Rushton <calielrushton@gmail.com>
2019-01-08 13:28:35 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,16 +17,16 @@ end
 
 def word_substituter(tweet)
   # takes in tweet, shortens it based on substitutes in dictionary method
-    new = [] # initialise an empty array to hold results of the loops to be performed...
-      tweet.split(" ").each do |word| # make tweet an array and loop over it:
-        dictionary.each do |k,v| # for each word in tweet, compare it to each key in hash within the dictionary method
-          if word.downcase == k.to_s # didn't realise case was a problem until testing next method
-            word = v # if a key matches, reassign the corresponding value to the word variable.
-          end
+  new = [] # initialise an empty array to hold results of the loops to be performed...
+    tweet.split(" ").each do |word| # make tweet an array and loop over it:
+      dictionary.each do |k,v| # for each word in tweet, compare it to each key in hash within the dictionary method
+        if word.downcase == k.to_s # didn't realise case was a problem until testing next method
+          word = v # if a key matches, reassign the corresponding value to the word variable.
         end
-        new << word # push the word into the new array, whether it was changed or not.  Exit the hash loop and go back to the next iteration of the array loop (until all words checked)
       end
-    new.join(" ") # return the new array as a string
+      new << word # push the word into the new array, whether it was changed or not.  Exit the hash loop and go back to the next iteration of the array loop (until all words checked)
+    end
+  new.join(" ") # return the new array as a string
 end
 # I found the idea of using .keys on the hash was a bit misleading here
 
@@ -46,7 +46,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   # truncates tweet to 140 chars with (...) if still too long after substitution
-  trunk_or_no = word_substituter(tweet)
+   trunk_or_no = word_substituter(tweet)
   if trunk_or_no.length > 140
     # truncate
       "#{trunk_or_no[0..135]}(..)"
